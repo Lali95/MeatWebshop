c136bf21a4149ac4359fa753d26b8daab171dc03
Lali95 <tothlajos.m@gmail.com>
2024-05-29 14:29:08 +0200

    feat: add sausage class

---
+++ Backend/Model/Sausage.cs
@@ -0,0 +1,20 @@
+namespace Backend.Model
+{
+    public class Sausage
+    {
+        
+        public int Id { get; set; }
+
+      
+        public string Name { get; set; }
+
+     
+        public string Type { get; set; }
+
+       
+        public float Weight { get; set; }
+
+      
+        public decimal Price { get; set; }
+    }
+}
